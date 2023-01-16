9ff9fbacfc8ee2173c3e7372871b3ff972b4b3d5
dev-brito <guilhermebrito2604@gmail.com>
2023-01-15 23:49:21 -0300

    Identificando Testes com o ID respectivo

--- src/test/java/teammates/logic/external/LocalLoggingServiceTest.java
+++ src/test/java/teammates/logic/external/LocalLoggingServiceTest.java
@@ -9,14 +9,17 @@ import teammates.test.BaseTestCase;
 public class LocalLoggingServiceTest extends BaseTestCase {
     @Test
     public void testIsRequestFilterSatisfied() {
+        //CT1
         assertTrue(new LocalLoggingService().isRequestFilterSatisfied(
                 null, null, null,
                 null, null, null, null));
 
+        //CT2
         assertFalse(new LocalLoggingService().isRequestFilterSatisfied(
                 new EmailSentLogDetails(), "ABC", null,
                 null, null, null, null));
 
+        //CT3
         RequestLogDetails requestLogDetails = new RequestLogDetails();
         requestLogDetails.setActionClass("abc");
         assertFalse(new LocalLoggingService().isRequestFilterSatisfied(
@@ -24,72 +27,84 @@ public class LocalLoggingServiceTest extends BaseTestCase {
                 null, null, null, null
         ));
 
+        //CT4
         assertTrue(new LocalLoggingService().isRequestFilterSatisfied(
                 requestLogDetails, "abc", null,
                 null, null, null, null
         ));
 
+        //CT5
         requestLogDetails.setResponseStatus(200);
         assertFalse(new LocalLoggingService().isRequestFilterSatisfied(
                 requestLogDetails, null, null, "201",
                 null, null, null
         ));
 
+        //CT6
         assertTrue(new LocalLoggingService().isRequestFilterSatisfied(
                 requestLogDetails, null, null, "200",
                 null, null, null
         ));
 
+        //CT7
         assertFalse(new LocalLoggingService().isRequestFilterSatisfied(
                 requestLogDetails, null, "abc 180", null,
                 null, null, null
         ));
 
-        //CT5
+        //CT8
         requestLogDetails.setResponseTime(200L);
         assertFalse(new LocalLoggingService().isRequestFilterSatisfied(
                 requestLogDetails, null, "> 250", null,
                 null, null, null
         ));
 
+        //CT9
         assertTrue(new LocalLoggingService().isRequestFilterSatisfied(
                 requestLogDetails, null, "> 180", null,
                 null, null, null
         ));
 
 
+        //CT10
         assertFalse(new LocalLoggingService().isRequestFilterSatisfied(
                 requestLogDetails, null, ">= 250", null,
                 null, null, null
         ));
 
+        //CT11
         assertTrue(new LocalLoggingService().isRequestFilterSatisfied(
                 requestLogDetails, null, ">= 200", null,
                 null, null, null
         ));
 
+        //CT12
         requestLogDetails.setResponseTime(250L);
         assertFalse(new LocalLoggingService().isRequestFilterSatisfied(
                 requestLogDetails, null, "< 250", null,
                 null, null, null
         ));
 
+        //CT13
         assertTrue(new LocalLoggingService().isRequestFilterSatisfied(
                 requestLogDetails, null, "< 251", null,
                 null, null, null
         ));
 
+        //CT14
         requestLogDetails.setResponseTime(251L);
         assertFalse(new LocalLoggingService().isRequestFilterSatisfied(
                 requestLogDetails, null, "<= 250", null,
                 null, null, null
         ));
 
+        //CT15
         assertTrue(new LocalLoggingService().isRequestFilterSatisfied(
                 requestLogDetails, null, "<= 251", null,
                 null, null, null
         ));
 
+        //CT16
         RequestLogUser userInfo = new RequestLogUser();
         userInfo.setRegkey("abc");
         requestLogDetails.setUserInfo(userInfo);
@@ -98,6 +113,7 @@ public class LocalLoggingServiceTest extends BaseTestCase {
                 "ab", null, null
         ));
 
+        //CT17
         assertTrue(new LocalLoggingService().isRequestFilterSatisfied(
                 requestLogDetails, null, null, null,
                 "abc", null, null
@@ -106,38 +122,45 @@ public class LocalLoggingServiceTest extends BaseTestCase {
 
 
         userInfo.setEmail("abc");
+        //CT18
         assertFalse(new LocalLoggingService().isRequestFilterSatisfied(
                 requestLogDetails, null, null, null,
                 null, "ab", null
         ));
 
+        //CT19
         assertTrue(new LocalLoggingService().isRequestFilterSatisfied(
                 requestLogDetails, null, null, null,
                 null, "abc", null
         ));
 
+        //CT20
         userInfo.setGoogleId("abc");
         assertFalse(new LocalLoggingService().isRequestFilterSatisfied(
                 requestLogDetails, null, null,
                 null, null, null, "ab"
         ));
 
+        //CT21
         assertTrue(new LocalLoggingService().isRequestFilterSatisfied(
                 requestLogDetails, null, null,
                 null, null, null, "abc"
         ));
 
+        //CT22
         requestLogDetails.setUserInfo(null);
         assertFalse(new LocalLoggingService().isRequestFilterSatisfied(
                 requestLogDetails, null, null, null,
                 "abc", null, null
         ));
 
+        //CT23
         assertFalse(new LocalLoggingService().isRequestFilterSatisfied(
                 requestLogDetails, null, null, null,
                 null, "abc", null
         ));
 
+        //CT24
         assertFalse(new LocalLoggingService().isRequestFilterSatisfied(
                 requestLogDetails, null, null, null,
                 null, null, "abc"
